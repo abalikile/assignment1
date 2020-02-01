49bf7a068d46014c75146a7c521e99af7319d26b
ash362 <60447294+ash362@users.noreply.github.com>
2020-02-01 13:16:41 -0800

    project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+const div1 = document.getElementById('contents');
+const profile = (
+<article>
+
+<section>
+<h2>Ashwini Balikile</h2>
+</section>
+
+
+<section>
+<img src={"image/IMG-1355 (1).JPG"} width="200" height="180"/>
+</section>
+
+<section>
+<p> Computer Science student at SDSU College of Extended Studies. Prior to this I did my Bachelor's in Computer Science from Visvesvaraya Technological University, India.</p>
+</section>
+
+<section>
+<p><a class="btn-primary btn-lg" href="https://github.com/abalikile/Module1_Assignment">VIEW MY GITHUB REPO</a></p>
+</section>
+
+
+</article>
+);
+ReactDOM.render(profile, div1);
